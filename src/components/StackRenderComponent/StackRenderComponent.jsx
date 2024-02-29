4fd96b41bd403a55cb1a9e52e117dd6d6ecd7e3e
valetronika <valentina2wo@gmail.com>
2024-02-29 12:46:43 +0100

    add StackRenderComponent component

---
+++ src/components/StackRenderComponent/StackRenderComponent.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import s from "./StackRenderComponent.module.scss";
+import PropTypes from "prop-types";
+export default function StackRenderComponent({ stacks_array }) {
+    console.log(stacks_array);
+    return (
+        <div className={s.stack}>
+            {stacks_array.map((stack) => (
+                <div className={s.stack_container} key={stack.id}>
+                    {stack}
+                </div>
+            ))}
+        </div>
+    );
+}
+
+StackRenderComponent.propTypes = {
+    stacks_array: PropTypes.array.isRequired,
+};
